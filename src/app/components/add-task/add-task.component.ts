0479bf88051b2088d3062637acdc4c980b207719
Paulius Mockevicius <pmockevicius@gmail.com>
2023-05-13 13:26:16 +0200

    add CRUD

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { Task } from "../../Task"
 
 @Component({
   selector: 'app-add-task',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent {
+@Output() onAddTask: EventEmitter<Task> = new EventEmitter()
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+  showAddTask!: boolean; 
+  subscription: Subscription;
+
+  constructor(private uiService: UiService){
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
+
+  onSubmit(){
+    if (!this.text){
+      alert ("Please add a task!")
+      return
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+
+    this.text ="";
+    this.day = "";
+    this.reminder = false;
+
+  }
 
 }
